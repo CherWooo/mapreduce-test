9d2afdd662738314a1696a56bb74038fc1c7a790
CherWooo <63425412+CherWooo@users.noreply.github.com>
2020-10-13 16:06:43 -0400

    Update reducer.py

--- mapreduce-test-python/top3/reducer.py
+++ mapreduce-test-python/top3/reducer.py
@@ -37,5 +37,5 @@ for hour_ip, count in dict_ip_count.items():
 for hour, ip_count in dict_hour_ip_count.items():
     top_3_ip = sorted(list(ip_count), key = lambda x:x[1], reverse=True)[0:3]
     print(hour,"top 3 ip", top_3_ip)
-    for ip, count in top_3_ip:
-        print ('%s\t%s' % ('['+hour+']' +ip, count))
+    #for ip, count in top_3_ip:
+        #print ('%s\t%s' % ('['+hour+']' +ip, count))
