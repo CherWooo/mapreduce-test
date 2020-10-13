065b4869c9ade687ce409b66ba64f835c7f75273
CherWooo <63425412+CherWooo@users.noreply.github.com>
2020-10-13 16:20:25 -0400

    Update test.sh

--- mapreduce-test-python/top3/test.sh
+++ mapreduce-test-python/top3/test.sh
@@ -3,7 +3,6 @@
 /usr/local/hadoop/bin/hdfs dfs -rm -r /top3/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /top3/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /top3/input/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /top3/output/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/access.log /top3/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
 -file ../../mapreduce-test-python/top3/mapper.py -mapper ../../mapreduce-test-python/top3/mapper.py \
