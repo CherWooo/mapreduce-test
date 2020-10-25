582128a798d20f140603cebc8cec3e48a6ba06c7
CherWooo <63425412+CherWooo@users.noreply.github.com>
2020-10-25 01:56:20 -0400

    Update mapper.py

--- mapreduce-test-python/logstat2/mapper.py
+++ mapreduce-test-python/logstat2/mapper.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python
-# --*-- coding:utf-8 --*--
-import re
 import sys
 
-pat = re.compile('(?P<ip>\d+.\d+.\d+.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
 for line in sys.stdin:
-    match = pat.search(line)
-    if match:
-        print '%s\t%s' % ('[' + match.group('hour') + ':00' + ']' + match.group('ip'), 1)
+    line = line.strip()
+    vtime = line.split(",")[19]
+       
+    if vtime != "Violation Time":
+        print('%s\t%s' % (vtime, '1'))
+              
+    elif vtime == "":
+        print('%s\t%s' % ("No Record", '1'))
