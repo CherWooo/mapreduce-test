5c77e8c4ffc21ab254cb7917e3c12d50993bf46e
CherWooo <63425412+CherWooo@users.noreply.github.com>
2020-10-25 01:56:47 -0400

    Update reducer.py

--- mapreduce-test-python/logstat2/reducer.py
+++ mapreduce-test-python/logstat2/reducer.py
@@ -1,42 +1,22 @@
-#!/usr/bin/python
-from operator import itemgetter
-import sys
+#!/usr/bin/env python
+# coding: utf-8
 
-dict_ip_count = {}
+import sys
+from operator import itemgetter
+
+dict_vtime_count = {}
 
 for line in sys.stdin:
     line = line.strip()
-    hour_ip, num = line.split('\t')
+    vtime, count = line.split('\t',1)
     try:
-        num = int(num)
-        dict_ip_count[hour_ip] = dict_ip_count.get(hour_ip, 0) + num
-
+        count = int(count)
+        dict_vtime_count[vtime] = dict_vtime_count.get(vtime, 0) + count
+        
     except ValueError:
         pass
+        
+sorted_dict_vtime_count = sorted(dict_vtime_count.items(), key=itemgetter(1))[::-1]
 
-dict_hour_ip_count = {}
-
-for hour_ip, count in dict_ip_count.items():
-        hour = hour_ip[1:6]
-        ip = hour_ip[7:]
-        count = int(count)
-        #print(hour)
-        #print(ip)
-        #print(count)
-        if hour not in dict_hour_ip_count.keys():
-            dict_hour_ip_count[hour] = [(ip,count)]
-        else:
-            dict_hour_ip_count[hour].append((ip, count))
-            
-for hour, ip_count in dict_hour_ip_count.items():
-    top_3_ip = sorted(list(ip_count), key = lambda x:x[1], reverse=True)[0:3]
-    print(hour,"top 3 ip", top_3_ip)
-
-for hour,ip_count in dict_hour_ip_count.items():
-    a = input('please enter a number: ')
-    if a+':00' not in dict_hour_ip_count.keys():
-        print('not found')
-    else:
-        top_3_ip = sorted(dict_hour_ip_count[a+':00'], key = lambda x:x[1], reverse=True)[0:3]
-        print(a+':00',top_3_ip)
-        break
+for vtime, count in sorted_dict_vtime_count:
+    print ('%s\t%s' % (vtime, count))
